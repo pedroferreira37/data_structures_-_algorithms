348550cc06497865baf08b6d7a4f479c8e60363f
Pedro Ferreira <pedrohsferreira1@gmail.com>
2023-07-13 09:22:49 -0300

    avl trees

---
+++ trees/avl/main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  struct Node *left;
+  int data;
+  int height;
+  struct Node *right;
+} Node;
+
+Node *root = NULL;
+
+int node_height(Node *p) {
+  int hl, hr;
+
+  hl = p && p->left ? p->left->height : 0;
+  hr = p && p->right ? p->right->height : 0;
+
+  return hl > hr ? hl + 1 : hr + 1;
+}
+
+int is_balanced(Node *p) {
+  int hl, hr;
+
+  hl = p && p->left ? p->left->height : 0;
+  hr = p && p->right ? p->right->height : 0;
+
+  return hl - hr;
+}
+
+Node *ll_rotation(Node *p) {
+  Node *pl = p->left;
+  Node *plr = pl->right;
+
+  pl->right = p;
+  p->left = plr;
+  p->height = node_height(p);
+  pl->height = node_height(pl);
+
+  if (root == p) {
+    root = pl;
+  }
+
+  return pl;
+}
+
+Node *lr_rotation(Node *p) {
+  Node *pl = p->left;
+  Node *plr = pl->right;
+
+  pl->right = plr->left;
+  p->left = plr->right;
+
+  plr->left = pl;
+  plr->right = p;
+
+  pl->height = node_height(pl);
+  p->height = node_height(p);
+  plr->height = node_height(plr);
+
+  if (root == p) {
+    root = plr;
+  }
+
+  return plr;
+}
+
+Node *rr_rotation(Node *p) { return NULL; }
+
+Node *rl_rotation(Node *p) { return NULL; }
+
+Node *RInsert(Node *p, int key) {
+  Node *t;
+
+  if (p == NULL) {
+    t = (Node *)malloc(sizeof(Node));
+    t->data = key;
+    t->height = 1;
+    t->left = t->right = NULL;
+    return t;
+  }
+  if (key < p->data) {
+    p->left = RInsert(p->left, key);
+  } else if (key > p->data) {
+    p->right = RInsert(p->right, key);
+  }
+
+  p->height = node_height(p);
+
+  if (is_balanced(p) == 2 && is_balanced(p->left) == 1) {
+    return ll_rotation(p);
+  } else if (is_balanced(p) == 2 && is_balanced(p->left) == -1) {
+    return lr_rotation(p);
+  } else if (is_balanced(p) == -2 && is_balanced(p->right) == -1) {
+    return rr_rotation(p);
+  } else if (is_balanced(p) == -2 && is_balanced(p->right) == 1) {
+    return rl_rotation(p);
+  }
+
+  return p;
+}
+
+void Preorder(Node *p) {
+
+  if (p) {
+    printf("%d ", p->data);
+    Preorder(p->left);
+    Preorder(p->right);
+  }
+}
+
+int main() {
+  root = RInsert(root, 50);
+  RInsert(root, 10);
+
+  RInsert(root, 20);
+
+  printf("%d", root->data);
+  return 0;
+}
